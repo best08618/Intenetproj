ed584f719050e68c7c660ed327c4d58d5febf143
best08618 <best08618@naver.com>
2018-06-15 17:02:21 +0900

    finalize

---
+++ app/src/main/obj/local/armeabi-v7a/objs/jnipiezo/piezo.o.d
@@ -0,0 +1,5 @@
+C:/Users/samsung/Desktop/Intenetproj/app/src/main/obj/local/armeabi-v7a/objs/jnipiezo/piezo.o: \
+  C:/Users/samsung/Desktop/Intenetproj/app/src/main/jni/piezo.cpp \
+  C:/Users/samsung/Desktop/Intenetproj/app/src/main/jni\com_example_jinji_internetproj_piano.h
+
+C:/Users/samsung/Desktop/Intenetproj/app/src/main/jni\com_example_jinji_internetproj_piano.h:
---
+++ app/src/main/obj/local/armeabi-v7a/objs/soundExample/sound.o.d
@@ -0,0 +1,5 @@
+C:/Users/samsung/Desktop/Intenetproj/app/src/main/obj/local/armeabi-v7a/objs/soundExample/sound.o: \
+  C:/Users/samsung/Desktop/Intenetproj/app/src/main/jni/sound.cpp \
+  C:/Users/samsung/Desktop/Intenetproj/app/src/main/jni\com_example_jinji_internetproj_ShowIpPort.h
+
+C:/Users/samsung/Desktop/Intenetproj/app/src/main/jni\com_example_jinji_internetproj_ShowIpPort.h:
