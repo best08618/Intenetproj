0c538f35107f081c0b0feea83c850c876b1266a2
best08618 <best08618@naver.com>
2018-06-15 18:16:32 +0900

    JINHWA ADD COMMIT ALL CODES

--- app/src/main/java/com/example/jinji/internetproj/testcode.java
+++ app/src/main/java/com/example/jinji/internetproj/testcode.java
@@ -9,8 +9,8 @@ import android.widget.Button;
 public class testcode extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.testpage1);
-        Button gotit = (Button) findViewById(R.id.gotbutton);
+        setContentView(R.layout.testpage1);  // test에 대해 설명해주는 page임
+        Button gotit = (Button) findViewById(R.id.gotbutton); // got it 버튼을 누르면 testmain으로 넘어가게 함
         gotit.setOnClickListener(new View.OnClickListener() {
                                      @Override
                                      public void onClick(View v) {
